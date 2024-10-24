d9f795f41a023482d9e75210e6bff36e06944215
Daniil Kucherenko <daniil.ukraine@gmail.com>
2024-10-24 23:34:06 +0300

    Edited mint_token.rs

--- mint_token.rs
+++ mint_token.rs
@@ -5,22 +5,21 @@ use solana_program::{
     msg,
     program_error::ProgramError,
     pubkey::Pubkey,
-    sysvar::{rent::Rent, Sysvar}
+    sysvar::{rent::Rent, Sysvar},
 };
 use spl_token::{
-    instruction::{initialize_mint, mint_to},
-    state::Mint,
+    instruction::{initialize_mint, mint_to, transfer},
+    state::Account as TokenAccount,
 };
-
 
 // Declare and export the program's entrypoint
 entrypoint!(process_instruction);
 
 // Program entrypoint's implementation
 pub fn process_instruction(
-    program_id: &Pubkey, // Public key of the account the hello world program was loaded into
-    accounts: &[AccountInfo], // The account to say hello to
-    instruction_data: &[u8], // Ignored, all helloworld instructions are hellos
+    program_id: &Pubkey, 
+    accounts: &[AccountInfo], 
+    instruction_data: &[u8], 
 ) -> ProgramResult {
     let instruction = instruction_data[0];
     match instruction {
@@ -30,13 +29,18 @@ pub fn process_instruction(
         }
         1 => {
             msg!("Mint");
-            mint_custom_usdc(program_id, accounts, instruction_data)?;
+            mint_custom_nft(program_id, accounts, instruction_data)?;
         }
-        _ => msg!("Invalid instruction")
+        2 => {
+            msg!("Attempting Transfer");
+            transfer_nft(program_id, accounts)?;
+        }
+        _ => msg!("Invalid instruction"),
     }
     Ok(())
 }
 
+// Function to initialize the NFT mint
 fn initialize_custom_mint(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
@@ -51,30 +55,53 @@ fn initialize_custom_mint(
         program_id, 
         mint_acc.key, 
         rent_acc.key, 
-        Some(freeze_authority.key),
-        6)?;
-        msg!("Mint initialized");
+        Some(freeze_authority.key), 
+        0,  // 0 decimal places since NFTs are non-fungible
+    )?;
+    
+    msg!("NFT mint initialized");
     Ok(())
 }
 
-fn mint_custom_usdc(
+// Function to mint the custom non-transferable NFT
+fn mint_custom_nft(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
     _instruction_data: &[u8],
 ) -> ProgramResult {
     let accounts_iter = &mut accounts.iter();
-    let mint_acc = next_account_info(accounts_iter)?; 
-    let reciever_acc = next_account_info(accounts_iter)?;
+    let mint_acc = next_account_info(accounts_iter)?;
+    let receiver_acc = next_account_info(accounts_iter)?;
     let mint_authority = next_account_info(accounts_iter)?;
-    let mint_amount = u64::from_le_bytes(_instruction_data[1..9].try_into().unwrap());
+
+    // Mint exactly 1 NFT
+    let mint_amount: u64 = 1;
+
     mint_to(
         program_id, 
         mint_acc.key, 
-        reciever_acc.key, 
+        receiver_acc.key, 
         mint_authority.key, 
         &[], 
-        mint_amount
+        mint_amount,
     )?;
-    msg!("Minted {}", mint_amount);
+
+    msg!("1 NFT minted to account {:?}", receiver_acc.key);
     Ok(())
 }
+
+// Function to prevent NFT transfers, ensuring non-transferability
+fn transfer_nft(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+) -> ProgramResult {
+    let accounts_iter = &mut accounts.iter();
+    let _source_acc = next_account_info(accounts_iter)?;
+    let _destination_acc = next_account_info(accounts_iter)?;
+    let _authority_acc = next_account_info(accounts_iter)?;
+
+    // Since this is a non-transferable NFT, we return an error when trying to transfer
+    msg!("Transfers are disabled for this NFT");
+    Err(ProgramError::Custom(1))  // Return custom error code indicating transfer not allowed
+}
+
